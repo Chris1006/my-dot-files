b987c60ffae7a684ddcd1a46c05ec1f9205573b0
christian poß <christianposs@gmx.de>
2020-01-08 20:36:16 +0100

    added xss-lock and removes gaps from i3

--- dotfiles/scripts/.scripts/utils/lockscreen.sh
+++ dotfiles/scripts/.scripts/utils/lockscreen.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 pkill -u "$USER" -USR1 dunst
-rm /tmp/screen_locked.png
+rm /tmp/screen_locked*.png
 scrot /tmp/screen_locked.png
-convert /tmp/screen_locked.png -scale 10% -scale 1000% /tmp/screen_locked2.png
+convert /tmp/screen_locked.png -scale 5% -scale 2000% /tmp/screen_locked2.png
 i3lock -n -e -c 000000 -u -i /tmp/screen_locked2.png
+rm /tmp/screen_locked*.png
 pkill -u "$USER" -USR2 dunst
+
+#loginctl lock-session
