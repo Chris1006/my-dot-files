830948f5ec5b07b7687998b828a17f7cc647bed2
christian poß <christianposs@gmx.de>
2021-08-19 11:57:38 +0200

    changed neovim settings, changed wallpaper, gruvbox sunset theme

---
+++ dotfiles/.config/nvim/README.md
@@ -0,0 +1,3 @@
+# neovim-config
+
+just some playing with neovim 0.5 with lps, treesitter, telescope and lua configuration.
---
+++ dotfiles/.config/nvim/init.lua
@@ -0,0 +1,15 @@
+require'common/settings'.setup()
+require'common/keymaps'
+
+require'common/plugins'.setup()
+require'layout/theme'
+
+require'plugins/nvim-lspconfig'
+require'plugins/nvim-lspsaga'
+require'plugins/nvim-treesitter'
+require'plugins/nvim-compe'
+require'plugins/nvim-dashboard'
+require'plugins/nvim-tree'
+require'plugins/nvim-telescope'
+require'plugins/barbar'
+require'plugins/lualine'
