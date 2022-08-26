26c6be2837f786abd530a878f1e9cdb033a45a04
christian poß <christianposs@gmx.de>
2022-08-26 14:14:04 +0200

    updated nvim config, fixed nvim tree and dashboard

--- dotfiles/.config/nvim/lua/common/plugins.lua
+++ dotfiles/.config/nvim/lua/common/plugins.lua
@@ -43,57 +43,12 @@
 
     -- file manager
     use {
-        'kyazdani42/nvim-tree.lua',
+    'kyazdani42/nvim-tree.lua',
         requires = 'kyazdani42/nvim-web-devicons',
         config = function() require'nvim-tree'.setup {
-disable_netrw       = true,
-  hijack_netrw        = true,
-  open_on_setup       = false,
-  ignore_ft_on_setup  = {},
-  auto_close          = false,
-  open_on_tab         = false,
-  hijack_cursor       = false,
-  update_cwd          = false,
-  update_to_buf_dir   = {
-    enable = true,
-    auto_open = true,
-  },
-  diagnostics = {
-    enable = false,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    }
-  },
-  update_focused_file = {
-    enable      = false,
-    update_cwd  = false,
-    ignore_list = {}
-  },
-  system_open = {
-    cmd  = nil,
-    args = {}
-  },
-  filters = {
-    dotfiles = false,
-    custom = {}
-  },
-  view = {
-    width = 30,
-    height = 30,
-    hide_root_folder = false,
-    side = 'left',
-    auto_resize = false,
-    mappings = {
-      custom_only = false,
-      list = {}
-    }
-  }
+        
         } end
     }
-
     -- better tab bar
     use 'romgrk/barbar.nvim'
 
@@ -109,7 +64,7 @@ disable_netrw       = true,
     use "lunarvim/colorschemes"
     use 'christianchiarulli/nvcode-color-schemes.vim'
     use "lunarvim/darkplus.nvim"
-
+    
     -- git stuff
     use {
       'lewis6991/gitsigns.nvim',
